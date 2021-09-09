efc0077895346345fc0b0833a1f8a24404a99605
Ivan Bolelov <ivan.bolelov@aarki.com>
2021-09-09 11:14:13 +0300

    fix: check

--- src/booster.rs
+++ src/booster.rs
@@ -195,32 +195,44 @@ impl Booster {
     /// Get Feature Names.
     pub fn feature_name(&self) -> Result<Vec<String>> {
         let num_feature = self.num_feature()?;
-        let feature_name_length = 0;
-        let mut num_feature_names = 0;
-        let mut out_buffer_len = 0;
-        let reserved_string_buffer_size = 255;
-        let reserved_string_buffer_size2: u64 = 255;
+        let mut tmp_out_len = 0;
+        let reserved_string_buffer_size: u64 = 255;
+        let reserved_string_buffer_size_usize = 255;
         let mut required_string_buffer_size = 0;
         let out_strs = (0..num_feature)
             .map(|_| {
-                CString::new(" ".repeat(reserved_string_buffer_size))
+                CString::new(" ".repeat(reserved_string_buffer_size_usize))
                     .unwrap()
                     .into_raw() as *mut c_char
             })
             .collect::<Vec<_>>();
-        println!("{:?}", out_strs);
         lgbm_call!(lightgbm_sys::LGBM_BoosterGetFeatureNames(
             self.handle,
-            // feature_name_length as i32,
-            // &mut num_feature_names,
             num_feature as i32,
-            &mut num_feature_names,
-            // num_feature as u64,
-            // &mut out_buffer_len,
-            reserved_string_buffer_size2 as u64,
+            &mut tmp_out_len,
+            reserved_string_buffer_size as u64,
             &mut required_string_buffer_size,
             out_strs.as_ptr() as *mut *mut c_char
         ))?;
+        let actual_string_buffer_size = required_string_buffer_size;
+        if actual_string_buffer_size > reserved_string_buffer_size {
+            let actual_string_buffer_size_usize= actual_string_buffer_size as usize;
+            let out_strs = (0..num_feature)
+                .map(|_| {
+                    CString::new(" ".repeat(actual_string_buffer_size_usize))
+                        .unwrap()
+                        .into_raw() as *mut c_char
+                })
+                .collect::<Vec<_>>();
+                lgbm_call!(lightgbm_sys::LGBM_BoosterGetFeatureNames(
+                    self.handle,
+                    num_feature as i32,
+                    &mut tmp_out_len,
+                    actual_string_buffer_size as u64,
+                    &mut required_string_buffer_size,
+                    out_strs.as_ptr() as *mut *mut c_char
+                ))?;
+        }
         let output: Vec<String> = out_strs
             .into_iter()
             .map(|s| unsafe { CString::from_raw(s).into_string().unwrap() })
